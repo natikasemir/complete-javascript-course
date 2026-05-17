958ea14bc3ce76d1857c8f7a8ff2163e70e722e6
Nati Kasemir <kasemir@kbw.de>
2026-05-17 18:59:13 +0200

    Added more content to the assignment.

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -15,3 +15,9 @@ console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+
+language = 'German';
+
+console.log(language);
+
+// Assignment 03
