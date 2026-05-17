482cfb559e51f7cae81a369516b6cfb6f758d6dd
Nati Kasemir <kasemir@kbw.de>
2026-05-17 18:50:02 +0200

    Added solution for 2nd assignment.

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -5,3 +5,13 @@ let continent = 'Europe';
 let population = 83000000;
 
 console.log(country, continent, population);
+
+// Assignment 02
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
