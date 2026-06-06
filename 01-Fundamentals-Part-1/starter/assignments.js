38bb8d4ef3fcdb198384298f1c9086764c09d778
Nati Kasemir <kasemir@kbw.de>
2026-06-06 22:18:29 +0200

    Added solution for assignment 07.

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -77,3 +77,12 @@ if (population > 33000000) {
 } else {
     console.log(`The population of ${country} is below average.`)
 }
+
+
+// Assignment #07
+
+console.log('9' - '5'); // -> 4
+console.log('19' - '13' + '17'); // -> 617
+console.log('19' - '13' + 17); // -> 23
+console.log('123' < 57); // -> false
+console.log(5 + 6 + '4' + 9 - 4 - 2); // ->1143
