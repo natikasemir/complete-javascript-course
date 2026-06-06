f3238810b09c56146db6649e4c65b29b4f555457
Nati Kasemir <kasemir@kbw.de>
2026-06-06 23:34:26 +0200

    Added solution for assignment 10.

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -111,3 +111,25 @@ if (language === 'English' && population < 50000000 && !isIsland) {
     console.log(`Sarah should live in Portugal. Or elsewhere. Nobody cares anyway.`)
 }
 
+
+// Assignment #10
+
+switch (language) {
+    case 'chinese' || 'mandarin':
+        console.log('MOST number of native speakers!')
+        break
+    case 'spanish':
+        console.log('2nd place in number of native speakers')
+        break
+    case 'english':
+        console.log('3rd place')
+        break
+    case 'hindi':
+        console.log('Number 4');
+        break
+    case 'arabic':
+        console.log('5th most spoken language')
+        break
+    default:
+        console.log('This is also a cool language, yes')
+}
