2a3ae49a66222790e3144230aa2a354a9b20d717
Nati Kasemir <kasemir@kbw.de>
2026-06-13 17:55:03 +0200

    Added solution for assignment 21.

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -145,6 +145,7 @@ const myCountry = {
     neighbours: ['Austria', 'Poland', 'Belgium', 'Switzerland', 'Czech Republic', 'Netherlands', 'Denmark', 'France', 'Luxembourg'],
 }
 
+
 // Assignment #19
 //Using the object from the previous assignment, log a string like this to the console: 'Finland has 6 million finnish-speaking people, 3 neighbouring countries and a capital called Helsinki'.
 //Increase the country's population by two million using dot notation, and then decrease it by two million using bracket notation.
@@ -158,6 +159,7 @@ const decreasePop = myCountry['population'] - 2000000
 console.log(decreasePop)
 
 //One can say: Dot-Notation is for absolute, while bracket notation is for computed.
+
 
 // Assignment #20
 
@@ -199,6 +201,7 @@ myCountry2.checkIsIsland()
 
 console.log(myCountry2)
 
+
 // Challenge #03
 
 const mark = {
@@ -228,3 +231,15 @@ const john = {
 john.calcBMI()
 
 console.log(`${john.fullName}s BMI (${john.bmi}) is (${john.bmi} > ${mark.bmi} ? 'higher' : 'lower';) than ${mark.fullName}s BMI (${mark.bmi})`)
+
+
+// Assignment #21
+
+// There are elections in your country! in a small town, there are only 50 voters. 
+// Use a for loop to simulate the 50 people voting, by logging a string like this to the console (for numbers 1 to 50): 'Voter number 1 is currently voting'.
+
+let votes
+
+for (votes = 1; votes <= 50; votes++) {
+    console.log(`Voter number ${votes} is currently voting.`)
+}
