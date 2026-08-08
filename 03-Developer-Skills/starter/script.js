0bc8bee6fde3766ee2486c2baa5e94d8429ee8fe
Nati Kasemir <193946486+natikasemir@users.noreply.github.com>
2026-08-08 21:54:53 +0200

    Added solution for coding challenge 2 of section 4.

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -32,9 +32,72 @@ const printForecast = function (arr) {
     console.log(`... ${str}`)
 }
 
-
 const data1 = [17, 21, 23]
 const data2 = [12, 5, -5, 0, 4]
 
 printForecast(data1)
 printForecast(data2)
+
+
+// Coding Challenge 2 (with or without AI)
+
+/* Let's say you're builidng a time tracking application for freelancers.
+At some point in building this app you need a function that receives daily work
+hours for a certain week and returns:
+1. Total hours worked
+2. Average daily hours
+3. The day with the most hours worked
+4. Numbers of days worked
+5. Whether the week was full time (worked 35 hours or more)
+
+Test data: [7.5, 8, 6.5, 0, 8.5, 4, 0]
+*/
+
+
+let nati = [7.5, 8, 6.5, 0, 8.5, 4, 0]
+
+const workStats = function (arr) {
+
+    // Total hours worked
+    
+    let sum = 0
+
+    for(let e of arr){
+        sum += e
+    }
+    console.log(`Total hours worked: ${sum} hours`)
+
+    // Average daily hours
+
+    let avg = 0
+    for(let i = 0; i < arr.length; i++) {
+        avg += arr[i]
+    }
+    avg = avg / arr.length
+    console.log(`Average working time: ${avg.toFixed(2)} hours`)
+
+    // The day with the most hours worked
+
+    console.log(`Maximum working time: ${Math.max(...arr)} hours`)
+
+    // Numbers of days worked
+
+    let daysWorked = 0
+
+    for(let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0){
+            daysWorked++
+        }
+    }
+    console.log(`Total days worked: ${daysWorked} days`)
+
+    // Full time or not (>35 hours)?
+
+    if (sum < 35) {
+        console.log(`Worked full time: No`)
+    } else {
+        console.log(`Worked full time: Yes`)
+    }
+}
+
+workStats(nati)
